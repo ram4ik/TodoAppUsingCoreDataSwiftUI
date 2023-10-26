724d7956492559ac13579ad066a4810c735b1834
ram4ik <ram4ik@hotmail.com>
2023-10-26 20:59:35 +0300

    Initial Commit

---
+++ TodoAppUsingCoreDataSwiftUI/TodoAppUsingCoreDataSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppUsingCoreDataSwiftUIApp.swift
+//  TodoAppUsingCoreDataSwiftUI
+//
+//  Created by admin on 10/26/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppUsingCoreDataSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
